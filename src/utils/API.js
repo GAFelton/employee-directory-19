0cda646e5f8d20e092b4b8f77f2376139554c4f1
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-11 15:25:16 -0800

    Changed results from 200 to 5 for testing purposes.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default function getUsers() { 
-  return axios.get('https://randomuser.me/api/?results=200&nat=us&inc=name,email,phone,picture');
+  return axios.get('https://randomuser.me/api/?results=5&nat=us&inc=name,email,phone,picture');
 };
