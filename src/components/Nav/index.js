f86cd85df27156aea66eb692163e208068ad0589
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-12 15:23:12 -0800

    Added dynamic button generation for sorting and query control to navbar.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,48 +1,63 @@
 import React from "react";
 
-function Nav() {
+function Nav({ query, onInputChange, onSortChange, currentKey, currentOrder }) {
+  const sorts = [
+    {
+      key: "firstName",
+      label: "First Name",
+    },
+    {
+      key: "lastName",
+      label: "Last Name",
+    },
+    {
+      key: "phone",
+      label: "Phone Number",
+    },
+  ];
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
-      <button className="navbar-brand bg-primary" href="/">
+    <nav className='navbar navbar-expand-lg navbar-dark bg-primary justify-content-between'>
+      <button className='navbar-brand bg-primary' href='/'>
         React Employee Directory
       </button>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarSupportedContent'
+        aria-controls='navbarSupportedContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <span class="navbar-text">Sort by: </span>
-          <li className="nav-item dropdown m-1">
-            <button className="btn dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              First Name
-        </button>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <button className="dropdown-item">Ascending</button>
-              <button className="dropdown-item">Descending</button>
-            </div>
-          </li>
-          <li className="nav-item dropdown m-1">
-            <button className="btn dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Last Name
-        </button>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <button className="dropdown-item">Ascending</button>
-              <button className="dropdown-item">Descending</button>
-            </div>
-          </li>
-          <li className="nav-item dropdown m-1">
-            <button className="btn dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Phone Number
-        </button>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <button className="dropdown-item">Ascending</button>
-              <button className="dropdown-item">Descending</button>
-            </div>
-          </li>
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul className='navbar-nav mr-auto'>
+          <span class='navbar-text'>Sort by: </span>
+          {sorts.map(({ key, label }) => (
+            <li key={key} className='nav-item dropdown m-1'>
+              <button
+                style={currentKey === key ? { color: "white" } : {}}
+                onClick={() =>
+                  onSortChange(key, currentOrder === "asc" ? "desc" : "asc")
+                }
+                className='dropdown-item'
+              >
+                {label} - {currentOrder === "asc" ? "Ascending" : "Descending"}
+              </button>
+            </li>
+          ))}
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <form className='form-inline my-2 my-lg-0'>
+          <input
+            onChange={onInputChange}
+            className='form-control mr-sm-2'
+            type='search'
+            value={query}
+            placeholder='Search'
+            aria-label='Search'
+          />
         </form>
       </div>
     </nav>
