060fddfd473ccae9bcd3160770ca4b5a53f52413
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-12 15:51:03 -0800

    Removed extra files and references from create-react-app.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export default {
+const getUsers = {
   getUsers: function () {
     return axios.get('https://randomuser.me/api/?results=5&nat=us&inc=name,email,phone,id,picture');
   }
 }
+
+export default getUsers;
