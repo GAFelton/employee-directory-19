5e88a1ee78ac33c657746035691e0d4337e62566
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-12 16:26:06 -0800

    Updated API call to pull 100 example user records.

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getUsers = {
   getUsers: function () {
-    return axios.get('https://randomuser.me/api/?results=5&nat=us&inc=name,email,phone,id,picture');
+    return axios.get('https://randomuser.me/api/?results=100&nat=us&inc=name,email,phone,id,picture');
   }
 }
 
